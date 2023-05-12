00e036fab90a739df09af8b3593278a75ae00bf6
vlad777442 <60163112+vlad777442@users.noreply.github.com>
2023-05-12 21:24:25 +0300

    initial commit

---
+++ src/main/java/com/example/vacationcalculatorneoflex/VacationCalculatorNeoflexApplication.java
@@ -0,0 +1,13 @@
+package com.example.vacationcalculatorneoflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationCalculatorNeoflexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacationCalculatorNeoflexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vacationcalculatorneoflex/VacationCalculatorNeoflexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vacationcalculatorneoflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationCalculatorNeoflexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
